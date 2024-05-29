311f042aeeba2f1a3425f0ba1eee73f4a2308ca2
Nick: The Dev <74797598+ntnicholson@users.noreply.github.com>
2024-05-29 08:38:43 -0500

    Initial Commit

---
+++ RockPaperScissors/RockPaperScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsApp.swift
+//  RockPaperScissors
+//
+//  Created by Nicholas Nicholson on 5/29/24.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
